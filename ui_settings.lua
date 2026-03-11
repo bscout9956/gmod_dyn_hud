c31ab4aeb7018b4254120395dc508462ab9af022
bscout9956 <bs.blackscout.bs@gmail.com>
2026-03-11 02:09:19 -0300

    Reduce settings panel size, it's humongous

--- ui_settings.lua
+++ ui_settings.lua
@@ -2,13 +2,13 @@ local UISettings = {}
 
 local SCREEN_WIDTH = ScrW()
 local SCREEN_HEIGHT = ScrH()
-UISettings.FRAME_WIDTH = 0.5 * SCREEN_WIDTH
-UISettings.FRAME_HEIGHT = 0.5 * SCREEN_HEIGHT
+UISettings.FRAME_WIDTH = 0.25 * SCREEN_WIDTH
+UISettings.FRAME_HEIGHT = 0.25 * SCREEN_HEIGHT
 
 UISettings.LEFT_MARGIN = 0.01 * SCREEN_WIDTH
 UISettings.TOP_MARGIN = 0.03 * SCREEN_HEIGHT
 
-local gridCountX, gridCountY = 2, 20
+local gridCountX, gridCountY = 2, 10
 UISettings.gridSizeX = UISettings.FRAME_WIDTH / gridCountX
 UISettings.gridSizeY = UISettings.FRAME_HEIGHT / gridCountY
 
