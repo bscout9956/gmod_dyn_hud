4d2c1bde9b6a5d818eef1de30f8ccb7484876476
bscout9956 <bs.blackscout.bs@gmail.com>
2026-03-18 22:59:12 -0300

    Update build script to add proper path

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Steam\steamapps\common\garrysmod\bin\gmad.exe" create -folder %1
+"C:\Program Files (x86)\Steam\steamapps\common\garrysmod\bin\gmad.exe" create -folder %1 -out %1/dynmap.gma
 pause
