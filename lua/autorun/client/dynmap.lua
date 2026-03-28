51e8d36f17430c21a733cac552a0286c800c9c23
bscout9956 <bs.blackscout.bs@gmail.com>
2026-03-28 15:06:42 -0300

    Move over code to an input handler

--- lua/autorun/client/dynmap.lua
+++ lua/autorun/client/dynmap.lua
@@ -38,24 +38,7 @@ function StartHooks()
 
     local nextChange = 0
     hook.Add("Think", "DynMap_Input", function()
-        local curTime = CurTime()
-        if input.IsKeyDown(KEY_Z) and curTime > nextChange then
-            State.ChangeZoom()
-            nextChange = curTime + 0.009
-        end
-
-        if input.IsKeyDown(KEY_X) and curTime > nextChange then
-            changeAstimagtismMode()
-            nextChange = curTime + 0.2
-        end
-        if input.IsKeyDown(KEY_M) and curTime > nextChange and Settings.settingsFramePresent == false then
-            Settings.OpenDynHudSettings()
-            nextChange = curTime + 0.5 -- We make it real slow lmao
-        end
-        if input.IsKeyDown(KEY_T) and curTime > nextChange then
-            switchMaps()
-            nextChange = curTime + 0.25
-        end
+        inputHandler.handleShortcuts(nextChange)
     end)
 
     if hooks["OnScreenSizeChanged"] then
---
+++ lua/autorun/client/dynmap/input.lua
@@ -0,0 +1,30 @@
+local InputHandler = {}
+
+local function changeAstimagtismMode()
+    Config.astigmatismMode = not Config.astigmatismMode
+end
+
+
+
+function InputHandler.handleShortcuts(nextChange)
+    local curTime = CurTime()
+    if input.IsKeyDown(KEY_Z) and curTime > nextChange then
+        State.ChangeZoom()
+        nextChange = curTime + 0.009
+    end
+
+    if input.IsKeyDown(KEY_X) and curTime > nextChange then
+        changeAstimagtismMode()
+        nextChange = curTime + 0.2
+    end
+    if input.IsKeyDown(KEY_M) and curTime > nextChange and Settings.settingsFramePresent == false then
+        Settings.OpenDynHudSettings()
+        nextChange = curTime + 0.5 -- We make it real slow lmao
+    end
+    if input.IsKeyDown(KEY_T) and curTime > nextChange then
+        State.switchMaps()
+        nextChange = curTime + 0.25
+    end
+end
+
+return InputHandler
